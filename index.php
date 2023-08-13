68893081c7f2c707f2c869138b059f87c693947a
shiv10701 <113887695+shiv10701@users.noreply.github.com>
2023-08-14 00:05:34 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Shivam Khandelwal</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Jost" rel="stylesheet">
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+		<style>
+            img[alt="www.000webhost.com"]{display:none;}
+        </style>
+</head>
+<body style="    background: linear-gradient(to bottom, #cc00ff -180%, #ffffff 40%);
+">
+<!--background:linear-gradient(to bottom right, #ff33cc -6%, #ff5050 100%);-->
+<div class="container-fluid">
+  <div class="row d-flex align-items-center justify-content-center" style="height:100vh;" data-aos="fade-up-right">
+    <div class="col-10 d-flex justify-content-center rounded" style="border:2px solid grey;background-color:rgba(255, 255, 255, 0.6);
+;max-width:450px;box-shadow: 1px 1px 15px 5px #CCCCCC;">
+    <div class="card" style="border:none;background:none;">
+    <div class="rounded-circle d-flex align-self-center align-items-center justify-content-center" style="margin-top:20px;height:170px;width:170px;border:1px solid #1D60A7;">
+  <img class="card-img-top rounded-circle d-flex align-self-center" src="self_images/shiv_sk.png" alt="Image of Shivam Khandelwal" style="padding:20px;height:200px;width:200px"></div>
+  <!--https://i.pinimg.com/originals/5c/b0/c9/5cb0c99158632e947f181bca583d156f-->
+  <div class="card-body" style="background-color:rgba(255, 255, 255, 0.6)">
+    <Span class="card-title d-flex justify-content-center" style="font-family:Jost;font-size:32px;margin-bottom:40px;">Shivam Khandelwal</Span>
+    
+  <!--  <div class="row d-flex justify-content-center" style="margin-bottom:30px;"><a class="d-flex justify-content-center" href="http://wa.me/+918087022677?text='Hello....Shivam'" style="text-decoration:none;color:black;"><div class="col-1"></div><div class="col-2" >
+    <img src="https://cdn-icons-png.flaticon.com/512/5968/5968841.png" style="height:40px;width:40px;"></div>
+    <div class="col-8"><span class="" style="font-size:20px;margin-left:30px;">WhatsApp</span></div>
+    </a>
+    </div>
+    
+    <div class="row d-flex justify-content-center" style="margin-bottom:30px;"><a class="d-flex justify-content-center" href="http://tel:8087022677" style="text-decoration:none;color:black;"><div class="col-1"></div><div class="col-2">
+    <img src="https://cdn-icons-png.flaticon.com/512/5415/5415999.png" style="height:40px;width:40px;"></div>
+    <div class="col-8"><span class="" style="font-size:20px;margin-left:30px;">Contact</span></div>
+    </a>
+    </div>
+    
+    
+    <div class="row d-flex justify-content-center"  style="margin-bottom:30px;"><a class="d-flex justify-content-center" href="#" style="text-decoration:none;color:black;"><div class="col-1"></div><div class="col-2">
+    <img src="https://cdn-icons-png.flaticon.com/512/1384/1384063.png" style="height:40px;width:40px;"></div>
+    <div class="col-8"><span class="" style="font-size:20px;margin-left:30px;">Instagram</span></div>
+    </a>
+    </div>  -->
+    
+    <div class="row d-flex justify-content-center " style="">
+    
+    <div class="col-4 d-flex justify-content-center" style="">
+    <a class="d-flex justify-content-center" href="http://wa.me/+918087022677?text='Hello....Shivam'" style="text-decoration:none;color:black;">
+    <img src="https://cdn-icons-png.flaticon.com/512/5968/5968841.png"  style="height:50px;width:50px;">
+    </a>
+    </div>
+    <div class="col-4 d-flex justify-content-center">
+    <a class="d-flex justify-content-center" href="tel:8087022677" style="text-decoration:none;color:black;">
+    <img src="https://cdn-icons-png.flaticon.com/512/5415/5415999.png" style="height:40px;width:40px;">
+    </a>
+    </div>
+    <div class="col-4 d-flex justify-content-center">
+    
+    <img src="https://cdn-icons-png.flaticon.com/512/1384/1384063.png" style="height:45px;width:45px;">
+    
+    </div>
+    </div>
+    
+    <div class="row d-flex jutify-content-center" style="padding-top:45px;padding-bottom:10px;">
+    <div class="col-12 d-flex justify-content-center">
+    <div class="rounded d-flex justify-content-center align-items-center shadow" style="background-color:#4F9EF1;width:40%;height:40px;"><span style="color:white;font-size:16px;">Message</span></div>
+    </div>
+    </div>
+    
+  </div>
+</div>
+    
+   
+  </div>
+
+</div>
+</div>
+</body>
+</html>
+
+  <script>
+  AOS.init();
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Shivam Khandelwal</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Jost" rel="stylesheet">
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+		<style>
+            img[alt="www.000webhost.com"]{display:none;}
+        </style>
+</head>
+<body style="    background: linear-gradient(to bottom, #66ccff -6%, #ffffff 44%);
+;
+">
+
+  <div class="row d-flex align-items-center justify-content-center" style="height:100vh;" data-aos="fade-up-right">
+    <div class="col-10 d-flex justify-content-center rounded" style="border:1px solid black;background-color:rgba(255, 255, 255, 0.8);
+;max-width:450px;box-shadow: 1px 1px 15px 5px #CCCCCC;">
+    <div class="card" style="border:none;background:none;">
+    <div class="rounded-circle d-flex align-self-center align-items-center justify-content-center" style="margin-top:20px;height:170px;width:170px; background:linear-gradient(to bottom right, #ff33cc -6%, #ff5050 100%);">
+  <img class="card-img-top rounded-circle d-flex align-self-center" src="https://i.pinimg.com/originals/5c/b0/c9/5cb0c99158632e947f181bca583d156f.jpg" alt="Image of Shivam Khandelwal" style="padding:20px;height:200px;width:200px"></div>
+  <div class="card-body" style="background-color:rgba(255, 255, 255, 0.8)">
+    <Span class="card-title d-flex justify-content-center" style="font-family:Jost;font-size:32px;margin-bottom:40px;">Shivam Khandelwal</Span>
+    
+    <div class="row d-flex justify-content-center" style="margin-bottom:30px;"><a class="d-flex justify-content-center" href="http://wa.me/+918087022677?text='Hello....Shivam'" style="text-decoration:none;color:black;"><div class="col-1"></div><div class="col-2" >
+    <img src="https://cdn-icons-png.flaticon.com/512/5968/5968841.png" style="height:40px;width:40px;"></div>
+    <div class="col-8"><span class="" style="font-size:20px;margin-left:30px;">WhatsApp</span></div>
+    </a>
+    </div>
+    
+    <div class="row d-flex justify-content-center" style="margin-bottom:30px;"><a class="d-flex justify-content-center" href="http://tel:8087022677" style="text-decoration:none;color:black;"><div class="col-1"></div><div class="col-2">
+    <img src="https://cdn-icons-png.flaticon.com/512/5415/5415999.png" style="height:40px;width:40px;"></div>
+    <div class="col-8"><span class="" style="font-size:20px;margin-left:30px;">Contact</span></div>
+    </a>
+    </div>
+    
+    
+    <div class="row d-flex justify-content-center"  style="margin-bottom:30px;"><a class="d-flex justify-content-center" href="#" style="text-decoration:none;color:black;"><div class="col-1"></div><div class="col-2">
+    <img src="https://cdn-icons-png.flaticon.com/512/1384/1384063.png" style="height:40px;width:40px;"></div>
+    <div class="col-8"><span class="" style="font-size:20px;margin-left:30px;">Instagram</span></div>
+    </a>
+    </div>
+    
+  </div>
+</div>
+    
+   
+  </div>
+
+</div>
+</body>
+</html>
+
+  <script>
+  AOS.init();
+</script>
+
+<!--
+
+    background: linear-gradient(to bottom right, #66ccff -6%, #ffffff 59%);
+    background: linear-gradient(to bottom, #66ccff -6%, #ffffff 44%);
+linear-gradient(to bottom right, #ccffff -6%, #ff5050 100%);
+
+linear-gradient(to bottom right, #ff33cc -6%, #ff5050 100%);
+background:linear-gradient(to bottom right, #ff0066 0%, #ffff00 100%);
+ background: linear-gradient(to bottom right, #ccccff 0%, #ffcccc 100%);
+-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Shivam Khandelwal</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Pacifico" rel="stylesheet">
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+		<style>
+            img[alt="www.000webhost.com"]{display:none;}
+        </style>
+</head>
+<body style="background-color:#FFFEE9;">
+
+  <div class="row d-flex align-items-center justify-content-center" style="height:100vh;" data-aos="fade-up-right">
+    <div class="col-10 d-flex justify-content-center rounded" style="background-color:white;max-width:450px;box-shadow: 1px 1px 15px 5px #CCCCCC;">
+    <div class="card" style="border:none;">
+  <img class="card-img-top rounded-circle d-flex align-self-center" src="https://i.pinimg.com/originals/5c/b0/c9/5cb0c99158632e947f181bca583d156f.jpg" alt="Image of Shivam Khandelwal" style="padding:20px;height:200px;width:200px">
+  <div class="card-body">
+    <Span class="card-title d-flex justify-content-center" style="font-family:pacifico;font-size:32px;margin-bottom:40px;">Shivam Khandelwal</Span>
+    
+    <div class="row d-flex justify-content-center" style="margin-bottom:30px;"><a class="d-flex justify-content-center" href="http://wa.me/+918087022677?text='Hello....Shivam'" style="text-decoration:none;color:black;"><div class="col-1"></div><div class="col-2" >
+    <img src="https://cdn-icons-png.flaticon.com/512/5968/5968841.png" style="height:40px;width:40px;"></div>
+    <div class="col-8"><span class="" style="font-size:20px;margin-left:30px;">WhatsApp</span></div>
+    </a>
+    </div>
+    
+    <div class="row d-flex justify-content-center" style="margin-bottom:30px;"><a class="d-flex justify-content-center" href="http://tel:8087022677" style="text-decoration:none;color:black;"><div class="col-1"></div><div class="col-2">
+    <img src="https://cdn-icons-png.flaticon.com/512/5415/5415999.png" style="height:40px;width:40px;"></div>
+    <div class="col-8"><span class="" style="font-size:20px;margin-left:30px;">Contact</span></div>
+    </a>
+    </div>
+    
+    
+    <div class="row d-flex justify-content-center"  style="margin-bottom:30px;"><a class="d-flex justify-content-center" href="#" style="text-decoration:none;color:black;"><div class="col-1"></div><div class="col-2">
+    <img src="https://cdn-icons-png.flaticon.com/512/1384/1384063.png" style="height:40px;width:40px;"></div>
+    <div class="col-8"><span class="" style="font-size:20px;margin-left:30px;">Instagram</span></div>
+    </a>
+    </div>
+    
+  </div>
+</div>
+    
+   
+  </div>
+
+</div>
+</body>
+</html>
+
+  <script>
+  AOS.init();
+</script>
+-->
